8d50a89ce117e61c6c8c2cafe1738aaff01d4a39
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-05-27 11:19:38 -0500

    feat nuevos datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String nombre1="julian santiago serna villada";
+        String nombre2="maria fernanda montezuma";
+        String nombre3="javier martinez";
+        String nombre4="jose jesus vargas martinez";
+        String nombre5="jacobo gonzales villegas";
+        String nombre6="andrea insignares";
+        String nombre7="diego alejandro osorno cardona";
+        int edad1="26";
+        int edad2="25";
+        int edad3="25";
+        int edad4="27";
+        int edad5="17";
+        int edad6="26";
+        int edad7="27";
+        int cedula1="1214747879";
+        int cedula2="1017272358";
+        int cedula3="1002371449";
+        int cedula4="1214742324";
+        int cedula5="1055360297";
+        int cedula6="1068684564";
+        int cedula7="1214742324";
+        String barrio1"castilla";
+        String barrio2"boston";
+        String barrio3="la magnolia";
+        String barrio4="prado";
+        String barrio5="betania";
+        String barrio6="belen";
+        String barrio7="manrique";
     }
 }
